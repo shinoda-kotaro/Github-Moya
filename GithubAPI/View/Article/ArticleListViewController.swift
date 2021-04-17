538a3d16ae5e2a3bcf2a91dae1e2796b7a1aaa16
shinoda-kotaro <shinoda.kotaro@arsaga.jp>
2021-04-17 16:32:36 +0900

    pod install Moya/RxSwift

--- GithubAPI/View/Article/ArticleListViewController.swift
+++ GithubAPI/View/Article/ArticleListViewController.swift
@@ -7,23 +7,34 @@
 
 import UIKit
 
-class ArticleListViewController: UIViewController {
-
+class ArticleListViewController: UIViewController  {
+    
+    @IBOutlet weak var articleListTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+        setTableView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setTableView() {
+        articleListTableView.delegate = self
+        articleListTableView.dataSource = self
+        articleListTableView.register(UINib(nibName: "ArticleTableViewCell", bundle: nil), forCellReuseIdentifier: "cell")
     }
-    */
 
+}
+
+
+extension ArticleListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "タイトル"
+        return cell
+    }
+    
 }
---
+++ Pods/Target Support Files/Pods-GithubAPI/Pods-GithubAPI-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GithubAPI : NSObject
+@end
+@implementation PodsDummy_Pods_GithubAPI
+@end
