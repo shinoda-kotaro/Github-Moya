3a532348e3fc6d284e3bcc16ce36aadf78a2ae17
shinoda-kotaro <shinoda.kotaro@arsaga.jp>
2021-04-17 16:32:19 +0900

    pod install Moya/RxSwift

---
+++ GithubAPI/View/Article/ArticleListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ArticleListViewController.swift
+//  GithubAPI
+//
+//  Created by 信田　虎太郎 on 2021/04/17.
+//
+
+import UIKit
+
+class ArticleListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ GithubAPI/View/Article/ArticleTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ArticleTableViewCell.swift
+//  GithubAPI
+//
+//  Created by 信田　虎太郎 on 2021/04/17.
+//
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
